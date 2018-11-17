f72ba2e1c8ae25042441f1f3bd1aac2498f4e18d
Tim Hoover <t.hoover8909@gmail.com>
2018-11-16 18:36:45 -0800

    Add files via upload

--- search.py
+++ search.py
@@ -1,11 +1,22 @@
 import pymongo
 
 my_client = pymongo.MongoClient("mongodb+srv://Rodriguez:0wE*RJ4d^7T6$pIU^tkY@cs121-searchdatabase-vsvf7.mongodb.net/test?retryWrites=true")
-mydb = my_client["search_index"]
+mydb = my_client["searchData"]
 mycol = mydb["tokens"]
 
-
+test_list = dict()
 term = input("Search: ")
 print("Looking that info up for you...")
-for i in mycol.find({"token": term}, {"token": 0, "freq": 0, "_id": 0}):
-    print(i)
+
+for i in mycol.find({"token": term}, {"_id": 0, "token": 0}):
+    test_list = i
+
+docID = list()
+urlList = list()
+
+#this is a stupid way to do this, let's make it better later
+for x in test_list.values():
+    for items in x.values():
+        if type(items) == str:
+            docID.append(items)
+
